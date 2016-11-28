cd3a1b0d43994942c0aee9326820441dd5a1704a
tonxery <tonxery@gmail.com>
2016-11-28 16:11:04 +0800

    q!

---
+++ phoneAndEmail.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import re,pyperclip
+phoneRegex = re.compile(r'''(
+    (\d{3}|\((\d{3})\))?
+    (\s|-|\.)?
+    (\d{3})
+    (\s|-|\.)
+    (\d{4})
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?
+
+                         )''',re.VERBOSE)
